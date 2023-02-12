0834a53e957053ff735e59b2ce85be9d96d28e2f
soravit_ice <61050302@kmitl.ac.th>
2023-02-12 16:37:12 +0700

    savedata and calculateProduct

--- store/models.py
+++ store/models.py
@@ -5,66 +5,71 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255 , blank=True)
+    slug = models.SlugField(max_length=255, blank=True)
 
     class Meta:
-        ordering = ['id'] #เรียงจากน้อยไปมาก ถ้า -id จะเป็นจากมากไปน้อย
+        ordering = ['id']  # เรียงจากน้อยไปมาก ถ้า -id จะเป็นจากมากไปน้อย
         verbose_name = 'หมวดหมู่สินค้า'
         verbose_name_plural = 'ข้อมูลหมวดหมู่สินค้า'
 
     def get_url(self):
-        return reverse('product_by_category' , args=[self.slug])
+        return reverse('product_by_category', args=[self.slug])
 
     def __str__(self):
         return self.name
-    
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, blank=True)
-    category = models.ForeignKey(Category, null=True , blank=True , on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, null=True, blank=True, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    image = models.ImageField(upload_to='product' )
+    image = models.ImageField(upload_to='product')
     barcode = models.CharField(max_length=255)
-    EXP = models.DateField() #วันหมดอายุ
-    created = models.DateTimeField(auto_now_add=True) #วัน-เวลาเพิ่มสินค้า
-    updated = models.DateTimeField(auto_now = True) #วัน-เวลาที่แก้ไขสินค้า
+    EXP = models.DateField()  # วันหมดอายุ
+    created = models.DateTimeField(auto_now_add=True)  # วัน-เวลาเพิ่มสินค้า
+    updated = models.DateTimeField(auto_now=True)  # วัน-เวลาที่แก้ไขสินค้า
 
-    class Meta: 
-        ordering = ['stock'] #เรียงจากน้อยไปมาก ถ้า -id จะเป็นจากมากไปน้อย
+    class Meta:
+        ordering = ['stock']  # เรียงจากน้อยไปมาก ถ้า -id จะเป็นจากมากไปน้อย
         verbose_name = 'คลังสินค้า'
         verbose_name_plural = 'ข้อมูลสินค้า'
 
     def show_image(self):
         if self.image:
-            return format_html('<img src= '+self.image.url+' height= "80px" >' )
+            return format_html('<img src= '+self.image.url+' height= "80px" >')
         return ''
     show_image.allow_tags = True
 
-    def __str__(self) :
+    def __str__(self):
         return self.name
 
+
 class Cart(models.Model):
-    cart_id = models.CharField(max_length=255,blank=True,null=True)
-    date_added = models.DateTimeField(auto_now_add=True) #วัน-เวลาเพิ่มขุ้อมูลสินค้า
+    cart_id = models.CharField(max_length=255, blank=True, null=True)
+    date_added = models.DateTimeField(
+        auto_now_add=True)  # วัน-เวลาเพิ่มขุ้อมูลสินค้า
 
     def __str__(self):
         return str(self.cart_id)
-    
+
     class Meta:
         db_table = 'cart'
         ordering = ['date_added']
         verbose_name = 'ตะกร้าสินค้า'
         verbose_name_plural = 'ข้อมูลตะกร้าสินค้า'
 
+
 class CartItem(models.Model):
-    product = models.ForeignKey(Product , on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     active = models.BooleanField(default=True)
 
@@ -72,13 +77,13 @@ class CartItem(models.Model):
         db_table = 'cartItem'
         verbose_name = 'รายการสินค้า'
         verbose_name_plural = 'ข้อมูลรายการสินค้า'
-        
-    #ผลรวมราคาขาย
+
+    # ผลรวมราคาขาย
     def sub_total(self):
         return self.product.price * self.quantity
-    
-    #ผลรวมต้นทุน
-    def sub_cost(self):   
+
+    # ผลรวมต้นทุน
+    def sub_cost(self):
         return self.product.cost * self.quantity
 
     def __str__(self):
@@ -86,40 +91,42 @@ class CartItem(models.Model):
 
 
 class Order(models.Model):
-    money=models.IntegerField() #รับเงิน
-    amount=models.DecimalField(max_digits=10,decimal_places=2 ,null=True) #เงินถอน
-    total=models.DecimalField(max_digits=10,decimal_places=2,null=True) #ราคาสินค้า
-    cost=models.DecimalField(max_digits=10,decimal_places=2,null=True) #ต้นทุนของสินค้า
-    profit=models.DecimalField(max_digits=10,decimal_places=2,null=True) #กำไรที่ได้
-    quantity = models.IntegerField()    
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
+    money = models.IntegerField()  # รับเงิน
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True)  # เงินถอน
+    total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True)  # ราคาสินค้า
+    cost = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True)  # ต้นทุนของสินค้า
+    profit = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True)  # กำไรที่ได้
+    quantity = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-    class Meta :
-        db_table='Order'
-        ordering=('created',)
+    class Meta:
+        db_table = 'Order'
+        ordering = ('created',)
         verbose_name = 'การซื้อสินค้า'
         verbose_name_plural = 'ข้อมูลการซื้อสินค้า'
 
 
 class OrderItem(models.Model):
-    product=models.CharField(max_length=250)
-    quantity=models.IntegerField()
-    price=models.DecimalField(max_digits=10,decimal_places=2)
-    order=models.ForeignKey(Order,on_delete=models.CASCADE ,null=True)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
-    
+    product = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-    class Meta :
-        db_table='OrderItem'
-        ordering=('created',)
+    class Meta:
+        db_table = 'OrderItem'
+        ordering = ('created',)
         verbose_name = 'รายการสินค้าที่ขาย'
         verbose_name_plural = 'ข้อมูลรายการสินค้าที่ขาย'
 
     def sub_total(self):
         return self.quantity*self.price
-    
+
     def __str__(self):
         return self.product
-        
--- store/views.py
+++ store/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from store.models import Category, Product, Cart, CartItem , Order , OrderItem
+from store.models import Category, Product, Cart, CartItem, Order, OrderItem
 from django.urls import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.auth import login , authenticate,logout
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import ProductForm , CategoryForm
+from .forms import ProductForm, CategoryForm
 from datetime import date
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 from io import StringIO, BytesIO
 
 # Create your views here.
-@login_required (login_url='signIn')
+
+
+@login_required(login_url='signIn')
 def pos(request, category_slug=None):
     product = None
     category_page = None
@@ -25,29 +27,28 @@ def pos(request, category_slug=None):
     else:
         product = Product.objects.all().filter()
 
-   
-
-    total = 0 #ยอดชำระ
-    cost = 0 #ยอดต้นทุน
-    counter = 0 #ยอดจำนวนสินค้าที่ซื้อ
-    profit = 0 #ยอดกำไรในการขาย
+    total = 0  # ยอดชำระ
+    cost = 0  # ยอดต้นทุน
+    counter = 0  # ยอดจำนวนสินค้าที่ซื้อ
+    profit = 0  # ยอดกำไรในการขาย
     cart_items = None
 
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))  # ดึงตะกร้าสินค้ามา
         cart_items = CartItem.objects.filter(
             cart=cart, active=True)  # ดึงข้อมูลสินต้าในตะกร้า
-        
+
         for item in cart_items:
             total += (item.product.price*item.quantity)
             cost += (item.product.cost*item.quantity)
             profit += total-cost
             counter += item.quantity
 
-        product = Product.objects.filter(name__icontains=request.GET ['title']) | Product.objects.filter(barcode__icontains=request.GET ['title'])
+        product = Product.objects.filter(name__icontains=request.GET['title']) | Product.objects.filter(
+            barcode__icontains=request.GET['title'])
     except Exception as e:
         pass
-    
+
     paginator = Paginator(product, 12)
     page = request.GET.get('page')
     try:
@@ -56,7 +57,7 @@ def pos(request, category_slug=None):
         product = paginator.page(1)
     except EmptyPage:
         product = paginator.page(paginator.num_pages)
-    
+
     return render(request, 'pos.html', {
         'product':  product,
         'category': category_page,
@@ -68,81 +69,82 @@ def pos(request, category_slug=None):
 
 
 def checkout_add(request):
-    
-    money = 0 #รับเงิน 
-    amount = 0#เงินถอน
-    total = 0 #ยอดชำระ
-    cost = 0 #ยอดต้นทุน
-    counter = 0 #ยอดจำนวนสินค้าที่ซื้อ
-    profit = 0 #ยอดกำไรในการขาย
+    money = 0  # รับเงิน
+    amount = 0  # เงินถอน
+    total = 0  # ยอดชำระ
+    cost = 0  # ยอดต้นทุน
+    counter = 0  # ยอดจำนวนสินค้าที่ซื้อ
+    profit = 0  # ยอดกำไรในการขาย
     cart_items = None
-
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))  # ดึงตะกร้าสินค้ามา
-        cart_items = CartItem.objects.filter(
+    print(request)
+    if request.method == "POST":
+        data = request.POST.copy()
+        print(data)
+        print("============POST=============")
+        try:
+            money = int(data.get('amount'))
+            total = int(float(data.get('total')))
+            print(money)
+            print(total)
+            amount = money - total
+            print(amount)
+            # บันทึกข้อมูลใบสั่งซื้อ
+            cart = Cart.objects.get(cart_id=_cart_id(request))  # ดึงตะกร้าสินค้ามา
+            cart_items = CartItem.objects.filter(
             cart=cart, active=True)  # ดึงข้อมูลสินต้าในตะกร้า
-        
-        for item in cart_items:
-            total += (item.product.price*item.quantity)
-            cost += (item.product.cost*item.quantity)
-            profit += total-cost
-            counter += item.quantity
-
-            
-        money = (int(request.GET["amount"])) 
-        amount = money - total
-
-       
-    except Exception as e:
-        pass
-
-        if request.method=="POST":
-            try:
-                #บันทึกข้อมูลใบสั่งซื้อ
-                order=Order.objects.create(
-                    total=total,
-                    cost=cost,
-                    profit=profit,
-                    quantity=counter
+            for item in cart_items:
+                cost += (item.product.cost*item.quantity)
+                counter += item.quantity
+            profit = total-cost
+            order = Order.objects.create(
+                money=money,
+                total=total,
+                cost=cost,
+                profit=profit,
+                quantity=counter,
+                amount = amount
+            )
+            order.save()
+            # บันทึกรายการสั่งซื้อ
+            for item in cart_items:
+                order_item = OrderItem.objects.create(
+                product=item.product.name,
+                quantity=item.quantity,
+                price=item.product.price,
+                order=order
                 )
-                order.save()
+                order_item.save()
+                # ลดจำนวน Stock
+                product = Product.objects.get(id=item.product.id)
+                product.stock = int(item.product.stock-order_item.quantity)
+                product.save()
+                item.delete()
+            return render(request, 'ch.html', {
+                'amount': amount
+            })
+        except Exception as e:
+            print("Error Post Method" , e)
+    else:
+        print("============GET=============")
+        try:
+            cart = Cart.objects.get(cart_id=_cart_id(request))  # ดึงตะกร้าสินค้ามา
+            cart_items = CartItem.objects.filter(
+            cart=cart, active=True)  # ดึงข้อมูลสินต้าในตะกร้า
 
-                #บันทึกรายการสั่งซื้อ
-                for item in cart_items :
-                    order_item=OrderItem.objects.create(
-                        product=item.product.name,
-                        quantity=item.quantity,
-                        price=item.product.price,
-                        order=order
-                    )
-                    order_item.save()
-                    #ลดจำนวน Stock
-                    product=Product.objects.get(id=item.product.id)
-                    product.stock=int(item.product.stock-order_item.quantity)
-                    product.save()
-                    item.delete()
-                return redirect('/')
-                
-            except Exception as e:
-                pass
-
-        return render(request, 'checkout_add.html',{
-        'cart_items': cart_items,
-        'total': total,
-        'profit': profit,
-        'cost': cost,
-        'counter': counter
-    }) 
-    return render(request, 'ch.html',{
-        'amount' : amount    
-    })
+            for item in cart_items:
+                total += (item.product.price*item.quantity)
+            return render(request, 'checkout_add.html', {
+            'total': total,
+        })
+        except Exception as e:
+            print("Exception Get" , e)
 
 
 def ch(request):
     return render(request, 'ch.html')
 
 
-def product(request , category_slug=None):
+def product(request, category_slug=None):
     product = None
     category_page = None
     today = date.today()
@@ -153,13 +155,12 @@ def product(request , category_slug=None):
     else:
         product = Product.objects.all().filter()
 
-
     try:
-        product = Product.objects.filter(name__icontains=request.GET ['title']) | Product.objects.filter(barcode__icontains=request.GET ['title']) 
+        product = Product.objects.filter(name__icontains=request.GET['title']) | Product.objects.filter(
+            barcode__icontains=request.GET['title'])
     except Exception as e:
         pass
 
-    
     paginator = Paginator(product, 12)
     page = request.GET.get('page')
     try:
@@ -168,18 +169,18 @@ def product(request , category_slug=None):
         product = paginator.page(1)
     except EmptyPage:
         product = paginator.page(paginator.num_pages)
-    
 
     return render(request, 'product.html', {
         'product':  product,
         'category': category_page,
-        'today':today
+        'today': today
     })
+
 
 def add_product(request):
     submitted = False
-    if request.method == "POST" :
-        form = ProductForm(request.POST , request.FILES)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/product?submitted=True')
@@ -187,43 +188,46 @@ def add_product(request):
         form = ProductForm
         if 'submitted' in request.GET:
             submitted = True
-    return render(request, 'add_product.html', {'form':form ,'submitted':submitted})
+    return render(request, 'add_product.html', {'form': form, 'submitted': submitted})
+
 
 def edit_product(request, product_id):
-    product = Product.objects.get(id = product_id) 
-    form = ProductForm(request.POST or None, request.FILES or None , instance=product)
+    product = Product.objects.get(id=product_id)
+    form = ProductForm(request.POST or None,
+                       request.FILES or None, instance=product)
     if form.is_valid():
-            form.save()
-            return redirect('product')
-    
+        form.save()
+        return redirect('product')
+
     return render(request, 'edit_product.html', {
-        'product':product,
-        'form':form
+        'product': product,
+        'form': form
     })
 
-def deleteProduct (request , product_id):
-    product = Product.objects.get(id = product_id)
+
+def deleteProduct(request, product_id):
+    product = Product.objects.get(id=product_id)
     product.delete()
     return redirect('product')
 
-def pdfProduct (request ):
+
+def pdfProduct(request):
     product = Product.objects.all()
 
     template_path = 'pdf_product.html'
     context = {'product': product}
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = ' filename="products_report.pdf"' #attachment; ดาวห์โหลดไฟล์
+    # attachment; ดาวห์โหลดไฟล์
+    response['Content-Disposition'] = ' filename="products_report.pdf"'
     template = get_template(template_path)
     html = template.render(context)
-    
 
     # create a pdf
-    pisa_status = pisa.CreatePDF(html.encode("UTF-8") ,dest=response)
-    # if error then show some funy view 
+    pisa_status = pisa.CreatePDF(html.encode("UTF-8"), dest=response)
+    # if error then show some funy view
     if pisa_status.err:
-       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return HttpResponse('We had some errors <pre>' + html + '</pre>')
     return response
-
 
 
 def index(request):
@@ -232,13 +236,14 @@ def index(request):
 
 def category(request):
     category = Category.objects.all().filter()
-    return render(request, 'category.html',{
-        'category' : category
+    return render(request, 'category.html', {
+        'category': category
     })
+
 
 def add_category(request):
     submitted = False
-    if request.method == "POST" :
+    if request.method == "POST":
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save()
@@ -247,53 +252,52 @@ def add_category(request):
         form = CategoryForm
         if 'submitted' in request.GET:
             submitted = True
-    return render(request, 'add_category.html', {'form':form ,'submitted':submitted})
+    return render(request, 'add_category.html', {'form': form, 'submitted': submitted})
 
 
-def deleteCategory (request , category_id):
-    category = Category.objects.get(id = category_id)
+def deleteCategory(request, category_id):
+    category = Category.objects.get(id=category_id)
     category.delete()
     return redirect('category')
 
+
 def edit_category(request, category_id):
-    category = Category.objects.get(id = category_id) 
+    category = Category.objects.get(id=category_id)
     form = CategoryForm(request.POST or None, instance=category)
     if form.is_valid():
-            form.save()
-            return redirect('category')
-    
+        form.save()
+        return redirect('category')
+
     return render(request, 'edit_category.html', {
         'category': category,
-        'form':form
+        'form': form
     })
-
-
 
 
 # สร้าง Session
 def _cart_id(request):
-   cart = request.session.session_key
-   if not cart:
-       cart = request.session.create()
-       return cart
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+        return cart
 
 
 def addCart(request, product_id):
     # มันส่งจะไอดีมาจากนั้น ไอดีจะเป็นตังดึงสินค้าออกมาตามรหัส
-    product = Product.objects.get(id=product_id) 
-    
+    product = Product.objects.get(id=product_id)
+
     # สร้างตะกร้าสินค้า
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-    except Cart.DoesNotExist:   
+    except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=_cart_id(request))
         cart.save()
         # บันทึกเข้าฐานข้อมูล
 
     try:
-        
+
         # ซื้อรายการสินค้าซ้ำ
-        cart_item = CartItem.objects.get(product=product, cart=cart , )
+        cart_item = CartItem.objects.get(product=product, cart=cart, )
         if cart_item.quantity < cart_item.product.stock:
             # เปลี่ยนจำนวนรายการสินค้า
             cart_item.quantity += 1
@@ -320,32 +324,32 @@ def removeCart(request, product_id):
     cartItem.delete()
     return redirect('/')
 
-def deleteCart (request):
+
+def deleteCart(request):
     cart = Cart.objects.get(cart_id=_cart_id(request))  # ดึงตะกร้าสินค้ามา
     cart.delete()
 
-    
     return redirect('/')
 
+
 def signInView(request):
-    if request.method=='POST':
-        form=AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            username=request.POST['username']
-            password=request.POST['password']
-            user=authenticate(username=username,password=password)
-            if user is not None :
-                login(request,user)
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
                 return redirect('pos')
-            else :
-                messages.error(request,"Invalid username or password.")
+            else:
+                messages.error(request, "Invalid username or password.")
     else:
-        messages.error(request,"Invalid username or password.")
-        form=AuthenticationForm()
-    return render(request,'signIn.html',{'form':form})
+        messages.error(request, "Invalid username or password.")
+        form = AuthenticationForm()
+    return render(request, 'signIn.html', {'form': form})
+
 
 def signOutView(request):
     logout(request)
     return redirect('signIn')
-
-
